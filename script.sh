9fe3c82ad027dc03e659d6c2a03b749ba29fa1ab
stenicole <87967040+stenicole@users.noreply.github.com>
2023-06-05 10:23:48 +0200

    Delete script.sh

--- script.sh
+++
@@ -1,4 +0,0 @@
-docker build --no-cache -f Dockerfile_fastapi -t fastapi_snicole:latest .
-docker build --no-cache -f Dockerfile_authent -t authent:latest .
-docker build --no-cache -f Dockerfile_authoriz -t authoriz:latest .
-docker build --no-cache -f Dockerfile_content -t content:latest .
